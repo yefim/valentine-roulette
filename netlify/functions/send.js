85f50a5eba9f72194257f0f361bb94490091ea74
Yefim Vedernikoff <yefim323@gmail.com>
2023-02-10 17:11:01 -0500

    Handle multipart form-date

--- netlify/functions/send.js
+++ netlify/functions/send.js
@@ -1,5 +1,62 @@
+import * as Busboy from "busboy"
+
+function parseMultipartForm(event) {
+  return new Promise((resolve) => {
+    // we'll store all form fields inside of this
+    const fields = {};
+
+    // let's instantiate our busboy instance!
+    const busboy = new Busboy({
+      // it uses request headers
+      // to extract the form boundary value (the ----WebKitFormBoundary thing)
+      headers: event.headers
+    });
+
+    // before parsing anything, we need to set up some handlers.
+    // whenever busboy comes across a file ...
+    busboy.on(
+      "file",
+      (fieldname, filestream, filename, _transferEncoding, mimeType) => {
+        // ... we take a look at the file's data ...
+        filestream.on("data", (data) => {
+          // ... and write the file's name, type and content into `fields`.
+          fields[fieldname] = {
+            filename,
+            type: mimeType,
+            content: data,
+          };
+        });
+      }
+    );
+
+    // whenever busboy comes across a normal field ...
+    busboy.on("field", (fieldName, value) => {
+      // ... we write its value into `fields`.
+      fields[fieldName] = value;
+    });
+
+    // once busboy is finished, we resolve the promise with the resulted fields.
+    busboy.on("finish", () => {
+      resolve(fields)
+    });
+
+    // now that all handlers are set up, we can finally start processing our request!
+    busboy.write(event.body);
+  });
+}
+
+
 const handler = async (event, _context) => {
-  console.log(event);
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 200,
+      body: 'Try sending a POST.'
+    };
+  }
+
+	const fields = await parseMultipartForm(event)
+
+  console.log(fields);
 
   return {
     statusCode: 200,
