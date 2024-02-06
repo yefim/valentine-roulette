2bd44a222183117529038a68632315db362b7db8
Yefim Vedernikoff <yefim323@gmail.com>
2024-02-05 22:35:37 -0500

    Add netlify proxy

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,7 +1,15 @@
 // https://github.com/parcel-bundler/parcel/issues/3407#issuecomment-686247350
 const serveStatic = require('serve-static');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Use static middleware
   app.use('/public', serveStatic('public'));
+
+  // Proxy to netlify dev -d app/build/ --debug
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'http://localhost:8888/',
+    }),
+  );
 };
